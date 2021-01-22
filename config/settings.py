e9e3626d7f6caeeda4e7cff5ca0b22972aecd8d1
hwanseok.dev <hwanseok.dev@gmail.com>
2021-01-22 14:06:52 +0900

    serialization tutorial

--- config/settings.py
+++ config/settings.py
@@ -19,16 +19,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-
+pass
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'snippets.apps.SnippetsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
