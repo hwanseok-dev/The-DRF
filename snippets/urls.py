a28001de7437079e40f9c7533b1a927e7808898b
hwanseok.dev <hwanseok.dev@gmail.com>
2021-01-23 18:09:00 +0900

    class based views

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,12 +1,10 @@
-from rest_framework.urlpatterns import format_suffix_patterns
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
-# We don't necessarily need to add these extra url patterns in,
-# but it gives us a simple, clean way of referring to a specific format.
 urlpatterns = format_suffix_patterns(urlpatterns)
